ac25608809eda8c3b9c5f0df4f806e4ea7cd4d58
Kyle Corry <kylecorry31@gmail.com>
2016-10-01 13:19:54 -0400

    Basic interpreter with add and remove

--- design.rkt
+++ design.rkt
@@ -1,12 +1,9 @@
-;; The first three lines of this file were inserted by DrRacket. They record metadata
-;; about the language level of this file in a form that our tools can easily process.
-#reader(lib "htdp-advanced-reader.ss" "lang")((modname design) (read-case-sensitive #t) (teachpacks ((lib "dir.rkt" "teachpack" "htdp") (lib "matrix.rkt" "teachpack" "htdp"))) (htdp-settings #(#t constructor repeating-decimal #t #t none #f ((lib "dir.rkt" "teachpack" "htdp") (lib "matrix.rkt" "teachpack" "htdp")) #t)))
 ;; Kyle Corry
 
 #|
 1. What kind of data is this language designed to process?
 
-   Shapes are the data. They have different sizes, shapes (circle or rectangle),
+   Shapes are the data. They have different sizes, shapes (circle or my-rect),
    and positions.
 
 
@@ -31,6 +28,8 @@
 
 |#
 
+(require "world-cs1102.rkt")
+
 ;; To be used on the canvas later, and for examples
 (define WIDTH 1000)
 (define HEIGHT 500)
@@ -39,9 +38,9 @@
 (define-struct animated-scene (shapes-used cmds))
 
 ;; a graphic-object is either
-;; - (make-rectangle posn number number symbol symbol), or
+;; - (make-my-rect posn number number symbol symbol), or
 ;; - (make-circle posn number symbol symbol)
-(define-struct rectangle (init-location width height color name))
+(define-struct my-rect (init-location width height color name))
 (define-struct circle (init-location radius color name))
 
 ;; a jump-posn is either
@@ -68,7 +67,7 @@
 (define-struct delta (x y))
 
 (define scene1 (let ([red-circle (make-circle (make-posn 20 40) 10 'red 'red-circle)]
-                     [blue-rect (make-rectangle (make-posn 160 40) 20 200 'blue 'blue-rect)])
+                     [blue-rect (make-my-rect (make-posn 160 40) 20 200 'blue 'blue-rect)])
                  (make-animated-scene
                   (list red-circle blue-rect)
                   (list (make-add-cmd 'red-circle)
@@ -88,8 +87,8 @@
                                                      empty)))))
 
 (define scene3 (let ([circ (make-circle (make-posn 40 20) 15 'orange 'circ)]
-                     [gr-rect (make-rectangle (make-posn 30 100) 150 20 'green 'gr-rect)]
-                     [red-rect (make-rectangle (make-posn 160 10) 20 80 'red 'red-rect)])
+                     [gr-rect (make-my-rect (make-posn 30 100) 150 20 'green 'gr-rect)]
+                     [red-rect (make-my-rect (make-posn 160 10) 20 80 'red 'red-rect)])
                  (make-animated-scene
                   (list circ gr-rect red-rect)
                   (list (make-add-cmd 'circ)
@@ -103,8 +102,8 @@
 
 
 (define scene4 (let ([my-circle (make-circle (make-posn 0 0) 10 'red 'my-circle)]
-                     [my-square (make-rectangle (make-posn 100 50) 10 10 'blue 'my-square)]
-                     [my-rect (make-rectangle (make-posn 50 100) 10 30 'green 'my-rect)])
+                     [my-square (make-my-rect (make-posn 100 50) 10 10 'blue 'my-square)]
+                     [my-rect (make-my-rect (make-posn 50 100) 10 30 'green 'my-rect)])
                  (make-animated-scene
                   (list my-circle my-square my-rect)
                   (list (make-add-cmd 'my-circle)
@@ -113,3 +112,153 @@
                         (make-do-until-collision-cmd 'my-circle 'my-square empty (list (make-add-cmd 'my-rect)
                                                                                        (make-do-until-collision-cmd 'my-circle 'my-rect empty
                                                                                                                     (list (make-remove-cmd 'my-circle)))))))))
+
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;; Interpreter ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; a shape-var is (make-shape-var symbol graphic-object boolean)
+(define-struct shape-var (name shape visible))
+
+;; a cmd-var is (make-cmd-var number cmd)
+(define-struct cmd-var (id cmd))
+
+(define init-shapes empty)
+
+(define cmd-queue empty)
+
+;; run-animation : animated-scene number number -> void
+;; Displays and runs an animated scene with the given width and height
+(define (run-animation an-animation width height)
+  (begin
+    (create-canvas width height)
+    (set-init-shapes (animated-scene-shapes-used an-animation))
+    (set-cmd-queue (animated-scene-cmds an-animation) 0)
+    (animation-loop (lambda (sn) (run-cmds cmd-queue sn)) (empty-scene width height))
+                  (void)))
+
+
+;; set-cmd-queue : list[cmd] number -> void
+;; (side-effect) Creates a variable for each of the cmds in a list of cmds starting from the starting id
+(define (set-cmd-queue cmds starting-id)
+  (cond[(empty? cmds) empty]
+       [(cons? cmds)
+        (begin (set! cmd-queue (append
+                                cmd-queue
+                                (list (make-cmd-var starting-id (first cmds)))))
+               (set-cmd-queue (rest cmds) (+ 1 starting-id)))]))
+                                       
+
+
+;; set-init-shapes : list[graphic-object] -> void
+;; (side-effect) Creates a variable for each of the shapes in a list of graphic objects
+(define (set-init-shapes lgo)
+  (cond[(empty? lgo) empty]
+       [(cons? lgo)
+        (begin (set! init-shapes (append init-shapes
+                (list (make-shape-var (cond[(circle? (first lgo)) (circle-name (first lgo))]
+                                           [(my-rect? (first lgo)) (my-rect-name (first lgo))])
+                                (first lgo)
+                                false))))
+               (set-init-shapes (rest lgo)))]))
+
+
+;; run-cmds : list[cmd] scene -> scene
+;; Runs all animation cmds in a list of cmds
+(define (run-cmds loc init-scene)
+  (cond[(empty? loc) init-scene]
+       [(cons? loc)
+        (run-cmds (rest loc) (run-cmd (first loc) init-scene))]))
+
+;; run-cmd : cmd-var scene -> scene
+;; Runs a cmd on the scene and (side-effect) removes the one time cmd from the queue or repeats a repetive cmd
+(define (run-cmd a-cmd a-scene)
+  (let [(cmd (cmd-var-cmd a-cmd))]
+  (cond [(add-cmd? cmd) (add-shape a-cmd a-scene)]
+        [(remove-cmd? cmd) (begin (remove-shape a-cmd)
+                                    a-scene)]
+        )))
+
+
+;; get-shape : symbol -> graphic-obj
+;; Gets the graphic obj from the init-shapes with the given name
+(define (get-shape name)
+  (shape-var-shape (first (filter (lambda (var)
+                   (symbol=? (shape-var-name var) name)) init-shapes))))
+
+;; add-shape : cmd-var scene -> scene
+;; Adds a graphic-object to the scene, (side-effect) removes the add-cmd from the queue
+(define (add-shape a-cmd a-scene)
+  (let [(graphic-obj (get-shape (add-cmd-shape (cmd-var-cmd a-cmd))))]
+    (let [(updated-scene (cond[(circle? graphic-obj) (draw-circle graphic-obj a-scene)]
+                              [(my-rect? graphic-obj) (draw-my-rect graphic-obj a-scene)]))]
+      (begin
+        
+        (set-visibility (cond[(circle? graphic-obj) (circle-name graphic-obj)]
+                             [(my-rect? graphic-obj) (my-rect-name graphic-obj)]
+                             )
+                              true)
+             ;;(remove-cmd-from-queue (cmd-var-id a-cmd))
+             updated-scene))))
+
+
+;; remove-shape : cmd-var -> void
+;; Sets the visibility of the given shape shape to false
+(define (remove-shape a-cmd)
+  (let [(graphic-obj (remove-cmd-shape (cmd-var-cmd a-cmd)))]
+    
+    (begin (set-visiblility graphic-obj
+                            false)
+           ;;(remove-cmd-from-queue (cmd-var-id a-cmd))
+           )))
+
+;; draw-circle : circle scene -> scene
+;; Draws a circle to the scene, returns the updated scene
+(define (draw-circle circ a-scene)
+  (place-image (ellipse (circle-radius circ)
+                        (circle-radius circ)
+                        'solid
+                        (circle-color circ))
+               (posn-x (circle-init-location circ))
+               (posn-y (circle-init-location circ))
+               a-scene))
+
+;; draw-my-rect : my-rect scene -> scene
+;; Draws a my-rect to the scene, returns the updated scene
+(define (draw-my-rect rect a-scene)
+  (place-image (rectangle (my-rect-width rect)
+                        (my-rect-height rect)
+                        'solid
+                        (my-rect-color rect))
+               (posn-x (my-rect-init-location rect))
+               (posn-y (my-rect-init-location rect))
+               a-scene))
+
+
+;; set-visibility : symbol boolean
+;; (side-effect) Sets the visibility of the shape with the given name in the init-shapes to the given visibility
+(define (set-visibility name visibility)
+  (set! init-shapes (map (lambda (sv)
+                           (cond[(symbol=? name (shape-var-name sv)) (make-shape-var (shape-var-name sv)
+                                                                                     (shape-var-shape sv)
+                                                                                     visibility)]
+                                [else sv]))
+                         init-shapes)))
+
+;; remove-cmd-from-queue : number -> void
+;; (side-effect) Removes the cmd with the given id from the cmd-queue
+(define (remove-cmd-from-queue id)
+  (set! cmd-queue (filter (lambda (cv)
+                            (not (= id (cmd-var-id cv)))
+                            ) cmd-queue)))
+                        
+
+
+
+
+
+
+(define (animation-loop update-fn init-scene)
+  (begin (update-frame (update-fn init-scene))
+         (sleep/yield 0.25)
+         (animation-loop update-fn init-scene)))
